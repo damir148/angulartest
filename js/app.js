a936492e8e046cc2d8e61ec58779b965c015b4a2
Damir Dimitrovici <damir.dimitrovici@gmail.com>
2014-06-16 17:51:13 +1000

    moved logic from html into seperate controller

--- js/app.js
+++ js/app.js
@@ -27,5 +27,14 @@
 	];
 	
 	app.controller('PanelController', function(){
+		this.tab = 1;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
 	});
 })();
